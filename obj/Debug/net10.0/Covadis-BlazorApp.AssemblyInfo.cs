8d7e42258aaaf585b4b704e20d4f5efe51d6a9b4
Robin Peter Miljoen <robin.miljoen@student.graafschapcollege.nl>
2026-04-21 09:25:24 +0200

    aaaaaaaaa

--- obj/Debug/net10.0/Covadis-BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/Covadis-BlazorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Covadis-BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2681887ed38174a40f01da7c45ad2be5525643a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e376488b031d652f83efc43ea51881dd9464a7df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Covadis-BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Covadis-BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
